894ac18b2b4737ad312205467a5c4d252b98dd43
Ronald Luo <ronaldluo93@gmail.com>
2026-03-23 14:44:58 +0000

    edit url and baseUrl in docusaurus configuration

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: 'http://students.cs.ucl.ac.uk',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/2025/group15/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
