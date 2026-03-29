38770b9852ec9bd5391a4d6344da8c9ceaa454b0
matthewchmok <matthewmok1125@gmail.com>
2026-03-29 18:30:15 +0100

    baseUrl fix

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   url: "http://students.cs.ucl.ac.uk",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/2025/group15/",
+  baseUrl: "./",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
